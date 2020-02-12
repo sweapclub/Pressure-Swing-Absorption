a17599ba2c67e95ddfa28f7a5cedcf9e93d11fc6
Nuttasak Munhadee <NUTTASMU@cementhai.com>
2020-02-12 15:44:50 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const express = require('express');
+const app = new express();
+
+app.use(express.static('dist/pressure-swing-absorption/'));
+app.get('/', function(request, response) {
+  // response.sendFile('absolutePathToYour/htmlPage.html');
+  response.sendFile('dist/pressure-swing-absorption/index.html', {
+    root: __dirname
+  });
+});
+
+const server = http.createServer(app);
+server.listen(8080, 'localhost', () =>
+  console.log('Moke server is on at : 8080')
+);
